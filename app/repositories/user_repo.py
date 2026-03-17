acc9d82f95a42f6be7133d9be1036d952ca5009a
M1Hato <miro28795@gmail.com>
2026-03-17 19:32:23 +0200

    Create function login_user_repo in UserRepo class. Write a validation for email and password when user login

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -1,7 +1,11 @@
+from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.security import verify_password
 from app.models.user_model import User
+from app.schemas.user_schemas import UserLogin
 
 
 class UserRepo:
@@ -22,3 +26,15 @@ class UserRepo:
             await session.rollback()
             return None
 
+    @staticmethod
+    async def login_user_repo(data: UserLogin, session: AsyncSession):
+        user = await UserRepo.get_user_by_email(data.email, session)
+        if user is None:
+            raise HTTPException(status_code=401, detail="Invalid Email or Password")
+
+        check_pwd = verify_password(user.password, data.password)
+
+        if not check_pwd:
+            raise HTTPException(status_code=401, detail="Invalid Email or Password")
+
+        return user
