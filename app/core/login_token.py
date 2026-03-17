01d5e804d50807668ff2d2b3f468344055ca9db4
M1Hato <miro28795@gmail.com>
2026-03-17 18:04:10 +0200

    Create new function for create access and refresh tokens after login in system

---
+++ app/core/login_token.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timezone, timedelta
+from jose import jwt
+from app.config import settings
+
+ACCESS_TOKEN = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+REFRESH_TOKEN = settings.REFRESH_TOKEN_EXPIRE_MINUTES
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+
+def create_access_token(data: dict):
+    payload = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN)
+    payload.update({"exp": expire})
+
+    encode_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
+def create_refresh_token(data: dict):
+    payload = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=REFRESH_TOKEN)
+    payload.update({"exp": expire})
+
+    encode_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
