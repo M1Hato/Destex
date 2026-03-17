f365bec241bafeeb2f2eb1f4dcdf7511dfb99589
M1Hato <miro28795@gmail.com>
2026-03-17 18:03:15 +0200

    Added new function login_user for login

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,10 +2,11 @@ from fastapi import HTTPException
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas.user_schemas import UserCreate
+from app.core.login_token import create_access_token, create_refresh_token
+from app.schemas.user_schemas import UserCreate, UserLogin
 from app.models.user_model import User
 from app.repositories.user_repo import UserRepo
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
 
 
 class AuthService:
@@ -15,7 +16,7 @@ class AuthService:
         data.password = hash_password(data.password)
 
         new_user_model = User(**data.model_dump())
-        result = await UserRepo.create_user(new_user_model, session)
+        result = await UserRepo.create_user_repo(new_user_model, session)
 
         if result is None:
             raise HTTPException(status_code=409, detail="User is already registered")
@@ -23,4 +24,21 @@ class AuthService:
         return result
 
 
+    @staticmethod
+    async def login_user(data: UserLogin, session: AsyncSession):
+        user = await UserRepo.get_user_by_email(data.email, session)
+        if user is None:
+            raise HTTPException(status_code=401, detail="Invalid Email or Password")
 
+        check_pwd = verify_password(user.password, data.password)
+
+        if not check_pwd:
+            raise HTTPException(status_code=401, detail="Invalid Email or Password")
+
+        access_token = create_access_token(data={"sub": user.email})
+        refresh_token = create_refresh_token(data={"sub": user.email})
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "bearer",
+        }
