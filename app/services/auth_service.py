1d014c7ec35874a6be2e7f7b440865662c483c04
M1Hato <miro28795@gmail.com>
2026-03-17 19:31:01 +0200

    Update function login_user

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -7,6 +7,7 @@ from app.schemas.user_schemas import UserCreate, UserLogin
 from app.models.user_model import User
 from app.repositories.user_repo import UserRepo
 from app.core.security import hash_password, verify_password
+from app.repositories.refresh_token_repo import RefreshTokenRepo
 
 
 class AuthService:
@@ -26,19 +27,12 @@ class AuthService:
 
     @staticmethod
     async def login_user(data: UserLogin, session: AsyncSession):
-        user = await UserRepo.get_user_by_email(data.email, session)
-        if user is None:
-            raise HTTPException(status_code=401, detail="Invalid Email or Password")
-
-        check_pwd = verify_password(user.password, data.password)
-
-        if not check_pwd:
-            raise HTTPException(status_code=401, detail="Invalid Email or Password")
+        user = await UserRepo.login_user_repo(data, session)
 
         access_token = create_access_token(data={"sub": user.email})
         refresh_token = create_refresh_token(data={"sub": user.email})
-        return {
-            "access_token": access_token,
-            "refresh_token": refresh_token,
-            "token_type": "bearer",
-        }
+        await RefreshTokenRepo.save_refresh_token(user.id, refresh_token, session)
+
+        #Збеженння в кукі soon
+
+        return {"access_token": access_token}
