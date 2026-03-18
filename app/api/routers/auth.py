8d83415989dd9e45baf82d6d3a2ecb51cbd45105
M1Hato <miro28795@gmail.com>
2026-03-19 01:24:05 +0200

    Write the new endpoint for logout user

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.requests import Request
+from starlette.responses import Response
 
 from app.database import get_async_session
 from app.schemas.user_schemas import UserCreate, UserLogin
@@ -21,6 +23,18 @@ async def register_user(
 @auth_router.post("/login")
 async def login_user(
         data: UserLogin,
+        response: Response,
         session: AsyncSession = Depends(get_async_session)
 ):
-    return await AuthService.login_user(data, session)
+    return await AuthService.login_user(data, response, session)
+
+@auth_router.post("/logout")
+async def logout_user(
+        response: Response,
+        request: Request,
+        session: AsyncSession = Depends(get_async_session)
+):
+    result = await AuthService.logout_user(request, session)
+
+    response.delete_cookie("refresh_token")
+    return result
