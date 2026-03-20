099884104a2b413f3f992258655d5c976dd2d570
M1Hato <miro28795@gmail.com>
2026-03-20 21:57:34 +0200

    added new relationship with task table

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.models.log_tokens import LogTokens
+from app.models.task_model import Task
 from app.database import Base
 
 class User(Base):
@@ -11,4 +12,5 @@ class User(Base):
     password: Mapped[str] = mapped_column(nullable=False)
 
     tokens: Mapped[list["LogTokens"]] = relationship(back_populates="user")
+    tasks: Mapped[list["Task"]] = relationship(back_populates="user")
 
