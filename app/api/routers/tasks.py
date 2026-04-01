2479de345ca640b798adfbe5b7a3c9c64defeab4
M1Hato <miro28795@gmail.com>
2026-04-02 02:42:53 +0300

    implement task searching by title and filtering by priority

--- app/api/routers/tasks.py
+++ app/api/routers/tasks.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.task_model import TaskPriority
 from app.services.task_service import TaskService
 from app.database import get_async_session
 from app.models.user_model import User
@@ -18,9 +20,11 @@ async def get_all_tasks(
         current_user: User = Depends(get_current_user),
         limit: int = Query(10, ge=1, le=10),
         offset: int = Query(0, ge=0),
+        search : Optional[str] = None,
+        priority: Optional[TaskPriority] = None,
         session: AsyncSession = Depends(get_async_session)
 ):
-    return await TaskService.get_task_service(current_user.id, limit, offset, session)
+    return await TaskService.get_task_service(current_user.id, limit, offset, search, priority, session)
 @task_router.post("/create", status_code = 201, response_model=TaskRead)
 async def create_task(
         data: TaskCreate,
