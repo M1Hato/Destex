851fa15bea3cf05bc1a15d6847f6850d3a488663
M1Hato <miro28795@gmail.com>
2026-04-02 01:41:34 +0300

    add tags, response_model and default status_code when successful completion

--- app/api/routers/tasks.py
+++ app/api/routers/tasks.py
@@ -1,22 +1,25 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.services.task_service import TaskService
 from app.database import get_async_session
 from app.models.user_model import User
-from app.schemas.task_schemas import TaskCreate, TaskUpdate
+from app.schemas.task_schemas import TaskCreate, TaskUpdate, TaskRead
 from app.api.deps import get_current_user
 
 task_router = APIRouter(
+    tags = ["Tasks"],
     prefix="/tasks",
 )
 
-@task_router.get("/all")
+@task_router.get("/all", response_model=List[TaskRead])
 async def get_all_tasks(
         current_user: User = Depends(get_current_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    return await TaskService.get_task_repo(current_user.id, session)
-@task_router.post("/create")
+    return await TaskService.get_task_service(current_user.id, session)
+@task_router.post("/create", status_code = 201, response_model=TaskRead)
 async def create_task(
         data: TaskCreate,
         current_user: User = Depends(get_current_user),
@@ -36,10 +39,11 @@ async def update_task(
     return await TaskService.update_task_service(data, current_user.id, task_id, session)
 
 
-@task_router.delete("/delete/{task_id}")
+@task_router.delete("/delete/{task_id}", status_code = 204)
 async def delete_task(
         task_id: int,
         current_user: User = Depends(get_current_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    return await TaskService.delete_task_service(task_id, current_user.id, session)
+    await TaskService.delete_task_service(task_id, current_user.id, session)
+    return None
