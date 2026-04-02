52f8e9cb8a1b5523aa416d4fda5fdded9074ed07
M1Hato <miro28795@gmail.com>
2026-04-02 17:53:47 +0300

    implement logging for all function in refresh_token_repo.py

--- app/repositories/refresh_token_repo.py
+++ app/repositories/refresh_token_repo.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone, timedelta
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.expression import update, select
 from app.config import settings
+from app.logging import logger
 from app.models.log_tokens import LogTokens
 
 REFRESH_TOKEN = settings.REFRESH_TOKEN_EXPIRE_MINUTES
@@ -29,6 +30,7 @@ class RefreshTokenRepo:
         session.add(token)
         await session.commit()
         await session.refresh(token)
+        logger.info(f"Refresh token has been saved for user: {user_id}")
         return token
 
 
@@ -36,6 +38,7 @@ class RefreshTokenRepo:
     async def deactivate_refresh_token(refresh_token: str, session: AsyncSession):
 
         if not refresh_token:
+            logger.warning("Refresh token for deactivate required")
             raise HTTPException(status_code=400, detail="Refresh token required")
 
         stmt = (
@@ -45,11 +48,13 @@ class RefreshTokenRepo:
         )
 
         await session.execute(stmt)
+        logger.info(f"Refresh token has been deactivated for user: {refresh_token}")
         await session.commit()
 
 
     @staticmethod
     async def get_token(refresh_token: str, session: AsyncSession):
+        logger.debug(f"Attempt to get refresh token for user: {refresh_token}")
         return await session.scalar(
             select(LogTokens)
             .where(LogTokens.refresh_token == refresh_token)
@@ -61,8 +66,9 @@ class RefreshTokenRepo:
         aware_now = datetime.now(timezone.utc).replace(tzinfo=None)
 
         if not token or not token.is_active or token.expires_at < aware_now:
+            logger.warning(f"Refresh token expired for user: {refresh_token}")
             raise HTTPException(status_code=401, detail="Refresh token is invalid or expired")
-
+        logger.info(f"Successfully get refresh token for user: {refresh_token}")
         return token
 
 
