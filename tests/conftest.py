252f4498a9856dff6ae3bc0ed0173ba9fa6057bc
M1Hato <miro28795@gmail.com>
2026-04-02 23:21:28 +0300

    write new database for test

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from app.database import Base
+
+TEST_BASE_URL = "sqlite+aiosqlite:///:memory:"
+engine = create_async_engine(TEST_BASE_URL)
+TestingSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+@pytest_asyncio.fixture
+async def db_session():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with TestingSessionLocal() as session:
+        yield session
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
